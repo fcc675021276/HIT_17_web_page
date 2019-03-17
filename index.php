5b9eca25f9320d788406c9f683858b68b1b30345
fcc675021276 <48525029+fcc675021276@users.noreply.github.com>
2019-03-17 09:24:17 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link type="text/css" rel="stylesheet" href="css/index.css">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name=”viewport” content=”width=device-width, initial-scale=1″ />
+<!–[if lt IE 9]><script src=”http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js”></script><![endif]–>
+
+<title>17级电信讲师团</title>
+</head>
+
+<body>
+<div id="all">
+	<h1> 欢迎来到哈尔滨工业大学17级电信讲师团</h1>
+	<h2> <a href="php/loginin.php" title="登录"> 登录 </a> </h2>
+	<h2> <a href="php/registe.php" title="注册"> 注册 </a> </h2>
+	<h2> <a href="php/bookcase.php" title="匿名访问"> 直接访问 </a> </h2>
+</div>
+</body>
+</html>
